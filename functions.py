2f6fe27edc8b86ac53a4ca46f3c1e86030abe507
WadiaSoft <1259281+WadiaSoft@users.noreply.github.com>
2024-12-05 12:11:17 -0800

    Initial commit

---
+++ functions.py
@@ -0,0 +1,16 @@
+FILE_PATH = "todos.txt"
+def get_todos(filepath=FILE_PATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath=FILE_PATH):
+    """ Write the to-do items l ist in the text file."""
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
+if __name__ == "__main__":
+    print("hello")
