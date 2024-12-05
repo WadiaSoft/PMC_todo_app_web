0b4506f7e8415ceab44e7d6ed481c7aaea13cc5b
WadiaSoft <1259281+WadiaSoft@users.noreply.github.com>
2024-12-05 12:40:00 -0800

    Update web.py removed experimental_ for deployment.

--- web.py
+++ web.py
@@ -18,7 +18,7 @@ for index, todo in enumerate(todos):
         todos.pop(index)
         functions.write_todos(todos)
         del st.session_state[todo]
-        st.experimental_rerun() #needed for checkboxes. At some point the experimental_ will be removed.
+        st.rerun() #needed for checkboxes. At some point the experimental_ will be removed.
 
 st.text_input(label="", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
